6eec5ea040267acd055c24b1ad843b4d840bf185
DohaEshra <95531188+DohaEshra@users.noreply.github.com>
2023-03-11 18:35:05 +0200

    Update location-serv.service.ts

--- src/user/location-serv/location-serv.service.ts
+++ src/user/location-serv/location-serv.service.ts
@@ -13,7 +13,7 @@ export class LocationServService {
     findLocation(latt, long) {
         return this.httpService.get('https://address-from-to-latitude-longitude.p.rapidapi.com/geolocationapi',{params: {lat: latt, lng: long},
         headers: {
-          'X-RapidAPI-Key': 'f41ae815b4msh44ed8e163ff0779p13a227jsne0af8baceef9',
+          'X-RapidAPI-Key': 'your-secret-key',
           'X-RapidAPI-Host': 'address-from-to-latitude-longitude.p.rapidapi.com'}});
     }
 
